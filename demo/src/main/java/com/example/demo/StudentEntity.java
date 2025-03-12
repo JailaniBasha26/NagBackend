a94727b01c43bedd45a039742441260ef031dd90
Basha <basha@brightcom.se>
2025-03-13 01:29:03 +0530

    first commit - BASHA

---
+++ demo/src/main/java/com/example/demo/StudentEntity.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ✅ Auto-increment
+    @Column
+    private int id;
+    private String name;
+    private String dept;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
